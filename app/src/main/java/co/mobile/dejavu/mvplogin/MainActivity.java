760e49cd73c6436a38b317d99fac1bf1fc108e84
josearangos <jose.arangos@udea.edu.co>
2019-04-05 16:30:28 -0500

    Created Presenter and View

--- app/src/main/java/co/mobile/dejavu/mvplogin/MainActivity.java
+++ app/src/main/java/co/mobile/dejavu/mvplogin/MainActivity.java
@@ -1,13 +1,57 @@
 package co.mobile.dejavu.mvplogin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import co.mobile.dejavu.mvplogin.Login.Presenter.ILoginPresenter;
+import co.mobile.dejavu.mvplogin.Login.Presenter.LoginPresenter;
+import co.mobile.dejavu.mvplogin.Login.View.ILoginView;
+import es.dmoral.toasty.Toasty;
+
+public class MainActivity extends AppCompatActivity implements ILoginView {
+
+
+    private EditText edtemail;
+    private EditText edtpassword;
+    private Button btnlogin;
+    private ILoginPresenter loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Init Views
+        this.btnlogin = (Button) findViewById(R.id.btn_login);
+        this.edtpassword = (EditText) findViewById(R.id.edt_password);
+        this.edtemail = (EditText) findViewById(R.id.edt_email);
+
+        //Init Presenter
+        loginPresenter = new LoginPresenter(this);
+
+        //Events
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginPresenter.onLogin(edtemail.getText().toString(),edtpassword.getText().toString());
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onLoginSuccess(String message) {
+        Toasty.success(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoginError(String message) {
+        Toasty.error(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/co/mobile/dejavu/mvplogin/Model/IUser.java
+++
@@ -1,9 +0,0 @@
-package co.mobile.dejavu.mvplogin.Model;
-
-public interface IUser {
-
-    String getEmail();
-    String getPassword();
-    boolean isValidData();
-
-}
